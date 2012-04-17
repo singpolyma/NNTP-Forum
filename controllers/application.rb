fee79a1b92397b084fed0a86cd9fedb7289b8f71
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-04-17 16:17:23 -0500

    Works when no results
    Specifically, 404

--- controllers/application.rb
+++ controllers/application.rb
@@ -29,27 +29,40 @@ class ApplicationController < HamlController
 		args[:content_type] = @req['_accept'] || @req.accept_media_types.select {|type| recognized_types.index(type) }.first
 		r = case args[:content_type]
 			when 'text/plain'
-				string = @threads.map {|thread|
-					body = thread[:body]
-					thread.delete(:body)
-					thread.delete(:mime)
-					thread.map { |k,v|
-						"#{k.to_s.gsub(/_/,'-')}: #{v}" if v
-					}.compact.join("\n") + "\n\n#{body}"
-				}.join("\n\n---\n")
+				if @threads
+					string = @threads.map {|thread|
+						body = thread[:text]
+						thread.delete(:text)
+						thread.delete(:body)
+						thread.delete(:mime)
+						thread.map { |k,v|
+							"#{k.to_s.gsub(/_/,'-')}: #{v}" if v
+						}.compact.join("\n") + "\n\n#{body}"
+					}.join("\n\n---\n")
+				else
+					string = super(args).last.to_s.gsub(/<[^>]+>/,' ')
+				end
 				[200, {'Content-Type' => 'text/plain; charset=utf-8'}, string]
 			when 'application/rss+xml'
-				[200, {'Content-Type' => 'application/rss+xml; charset=utf-8'}, self.include('views/rss.haml')]
+				if @threads
+					[200, {'Content-Type' => 'application/rss+xml; charset=utf-8'}, self.include('views/rss.haml')]
+				else
+					[404, {'Content-Type' => 'application/rss+xml; charset=utf-8'}, '<rss/>']
+				end
 			when 'application/json'
-				@threads.each {|thread| thread.delete(:mime)}
-				[200, {'Content-Type' => 'application/json; charset=utf-8'}, @threads.to_json]
+				if @threads
+					@threads.each {|thread| thread.delete(:mime)}
+					[200, {'Content-Type' => 'application/json; charset=utf-8'}, @threads.to_json]
+				else
+					[404, {'Content-Type' => 'application/json; charset=utf-8'}, '{}']
+				end
 			else
 				args[:content_type] += '; charset=utf-8' if args[:content_type]
 				super(args)
 		end
 		# Cache headers. Varnish likes Cache-Control.
-		last_modified = (@threads.map {|thread| thread[:date]}.sort.last + 240)
-		r[1].merge!({'Vary' => 'Accept', 'Cache-Control' => 'public, max-age=240', 'Last-Modified' => last_modified.rfc2822})
+		last_modified = ((@threads || []).map {|thread| thread[:date]}.sort.last || Time.now)
+		r[1].merge!({'Vary' => 'Accept', 'Cache-Control' => 'public, max-age=240', 'Last-Modified' => (last_modified + 240).rfc2822})
 		r
 	end
 end
