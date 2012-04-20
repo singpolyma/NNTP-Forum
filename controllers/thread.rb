e5162e1bc88d8f851caeb2c072d8204941b5b238
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-04-20 16:00:09 -0500

    better nested blockquote support

--- controllers/thread.rb
+++ controllers/thread.rb
@@ -59,7 +59,9 @@ class ThreadController < ApplicationController
 					}
 					thread[:body] = doc.at('body').to_xhtml(:encoding => 'UTF-8').sub(/^<body>/, '').sub(/<\/body>$/, '').force_encoding('UTF-8')
 				else
-					thread[:body] = thread[:text].gsub(/</,'&lt;').gsub(/(^[^>].*\n)>/, "\\1\n\n>")
+					thread[:body] = thread[:text].gsub(/</,'&lt;')
+					thread[:body] = thread[:body].gsub(/^>[ >]*/) {|s| s.gsub(/ /,'') + ' '}
+					thread[:body] = thread[:body].gsub(/(^[^>].*\n)>/, "\\1\n\n>").gsub(/^(>+)(.*)\n(\1>)/,"\\1\\2\n\\1\n\\3")
 					thread[:body] = Kramdown::Document.new(thread[:body]).to_html.encode('UTF-8')
 				end
 
